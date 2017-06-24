49c8df2a732e6ebfd866c39afc9f4f1efd4a90a1
Gladys Um <gladysum@gmail.com>
2017-06-24 02:29:25 -0400

    styled organize page

--- browser/Organize.js
+++ browser/Organize.js
@@ -97,130 +97,137 @@ class Organize extends React.Component {
                 Before filling out the Food Drive A Tron, make sure you've completed steps 1, 2, and 3! Once you submit your food drive info, the Food Drive A Tron will kick off a series of automated processes that will send messages announcing your event to all your contacts. The messages will include a link to a website where people can sign up to volunteer. The Food Drive A Tron will also send reminder messages to the volunteers.
               </div>
               <div className="question">
-                Step 5: Go to Successfully Planned Food Drive!
+                Step 5: Go to Your Successfully Planned Food Drive!
               </div>
               <div className="answer">
                 Show up at the venue on the day of your food drive and have a great time!
               </div>
+              <hr/>
+              <div className="form-title">
+                Food Drive A Tron
+              </div>
+
+              {this.props.organizermodal ? <OrganizerModal/> : null}
+              <form onSubmit={this.handleSubmit}>
+
+                <label>
+                  Name of food drive:
+                  <input
+                    name="fooddrivename"
+                    type="text"
+                    value={this.state.fooddrivename}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Your name:
+                  <input
+                    name="organizername"
+                    type="text"
+                    value={this.state.organizername}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Your email:
+                  <input
+                    name="organizeremail"
+                    type="text"
+                    value={this.state.organizeremail}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Month:
+                  <input
+                    name="month"
+                    type="text"
+                    value={this.state.month}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Date:
+                  <input
+                    name="date"
+                    type="text"
+                    value={this.state.date}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Year:
+                  <input
+                    name="year"
+                    type="text"
+                    value={this.state.year}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  Venue:
+                  <input
+                    name="venue"
+                    type="text"
+                    value={this.state.venue}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  streetaddress:
+                  <input
+                    name="streetaddress"
+                    type="text"
+                    value={this.state.streetaddress}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  town:
+                  <input
+                    name="town"
+                    type="text"
+                    value={this.state.town}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  state:
+                  <input
+                    name="state"
+                    type="text"
+                    value={this.state.state}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <label>
+                  zipcode:
+                  <input
+                    name="zipcode"
+                    type="text"
+                    value={this.state.zipcode}
+                    onChange={this.handleChange} />
+                </label>
+                <br/>
+
+                <br/>
+                <input type="submit" value="Submit" />
+              </form>
+
           </div>
 
 
-          {this.props.organizermodal ? <OrganizerModal/> : null}
-          <form onSubmit={this.handleSubmit}>
 
-            <label>
-              Name of food drive:
-              <input
-                name="fooddrivename"
-                type="text"
-                value={this.state.fooddrivename}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Your name:
-              <input
-                name="organizername"
-                type="text"
-                value={this.state.organizername}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Your email:
-              <input
-                name="organizeremail"
-                type="text"
-                value={this.state.organizeremail}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Month:
-              <input
-                name="month"
-                type="text"
-                value={this.state.month}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Date:
-              <input
-                name="date"
-                type="text"
-                value={this.state.date}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Year:
-              <input
-                name="year"
-                type="text"
-                value={this.state.year}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              Venue:
-              <input
-                name="venue"
-                type="text"
-                value={this.state.venue}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              streetaddress:
-              <input
-                name="streetaddress"
-                type="text"
-                value={this.state.streetaddress}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              town:
-              <input
-                name="town"
-                type="text"
-                value={this.state.town}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              state:
-              <input
-                name="state"
-                type="text"
-                value={this.state.state}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <label>
-              zipcode:
-              <input
-                name="zipcode"
-                type="text"
-                value={this.state.zipcode}
-                onChange={this.handleChange} />
-            </label>
-            <br/>
-
-            <br/>
-            <input type="submit" value="Submit" />
-          </form>
         </div>
       </div>
     )
